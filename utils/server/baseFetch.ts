8270c2f92db850eb7bdb822a7d5ded231ebe83a6
Anton <antklim89@mail.ru>
2022-06-08 02:09:07 +0300

    updated error handle

--- utils/server/baseFetch.ts
+++ utils/server/baseFetch.ts
@@ -1,8 +1,8 @@
 import { STRAPI_URL } from '~/constants';
 
 
-interface BaseFetchArgs<Q, V> {
-    query: Q
+interface BaseFetchArgs<V> {
+    query: string
     variables?: V
 }
 
@@ -10,16 +10,19 @@ const GRAPHQL_URL = `${STRAPI_URL}/graphql`;
 
 
 export default async function baseFetch<
-    T = unknown, Q = string, V = Record<string, string>
->({ query, variables }: BaseFetchArgs<Q, V>): Promise<T> {
-
-    const resp = await fetch(GRAPHQL_URL, {
+    T = unknown, V = Record<string, string>
+>({ query, variables }: BaseFetchArgs<V>): Promise<T> {
+    const response = await fetch(GRAPHQL_URL, {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify({ query, variables }),
     });
 
-    const result = await resp.json();
+    const result = await response.json();
+
+    if (result.errors) {
+        throw new Error(result.errors[0].message);
+    }
 
     return result.data;
 }
