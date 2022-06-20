e27f8275c9c0a409f02b2fd44b84f96764f54d6f
Anton <antklim89@mail.ru>
2022-06-20 21:05:12 +0300

    updated Ad query

--- types/Ad.ts
+++ types/Ad.ts
@@ -2,14 +2,10 @@
 
 export interface IAd {
     id: string;
-    name?: string;
     birthday: string;
     type: string;
-    body: string;
     breed?: string;
-    email: string;
     images: string[]
     price: number;
-    tel: string;
     createdAt: string;
 }
