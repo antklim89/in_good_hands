1f92aa775c93d5759f116c62cab88a49687768a6
Anton <antklim89@mail.ru>
2022-06-20 22:36:32 +0300

    updated EditAd component

---
+++ layouts/EditAd/EditAd.schema.ts
@@ -0,0 +1,28 @@
+import zod from 'zod';
+
+import schema from '~/strapi/src/api/ad/content-types/ad/schema.json';
+
+
+export const adEditSchema = zod.object({
+    body: zod.string()
+        .min(schema.attributes.body.minLength)
+        .max(schema.attributes.body.maxLength),
+    type: zod.enum(schema.attributes.type.enum as [string, ...string[]]),
+    breed: zod.string()
+        .min(schema.attributes.breed.minLength)
+        .max(schema.attributes.breed.maxLength),
+    name: zod.string()
+        .min(schema.attributes.name.minLength)
+        .max(schema.attributes.name.maxLength),
+    price: zod.number()
+        .min(schema.attributes.price.min)
+        .max(schema.attributes.price.max),
+    tel: zod.string()
+        .min(schema.attributes.tel.minLength)
+        .max(schema.attributes.tel.maxLength),
+    email: zod.string()
+        .email()
+        .min(schema.attributes.email.minLength)
+        .max(schema.attributes.email.maxLength),
+    birthday: zod.date(),
+});
--- layouts/EditAd/EditAd.tsx
+++ layouts/EditAd/EditAd.tsx
@@ -1,16 +1,27 @@
 import { Center, Container, Heading, Flex, Button } from '@chakra-ui/react';
 import { useFormik } from 'formik';
 import { FC } from 'react';
+import { ZodError } from 'zod';
 
 import InputField from '~/components/InputField';
 
+import { adEditSchema } from './EditAd.schema';
 import { CreateAdProps } from './EditAd.types';
 
 
 const EditAd: FC<CreateAdProps> = ({ type = 'create' }) => {
 
     const formik = useFormik({
-        initialValues: { username: '', email: '', password: '', confirm: '' },
+        initialValues: {
+            body: '',
+            type: '',
+            breed: '',
+            name: '',
+            price: '0',
+            tel: '',
+            email: '',
+            birthday: new Date().toISOString().split('T')[0],
+        },
         async onSubmit(val) {
             // try {
             //         const user = await login(val);
@@ -20,11 +31,11 @@ const EditAd: FC<CreateAdProps> = ({ type = 'create' }) => {
             // }
         },
         async validate(val) {
-            // try {
-            //         await authSchema.pick({ email: true, password: true, username: true }).parseAsync(val);
-            // } catch (error) {
-            //     if (error instanceof ZodError) return error.formErrors.fieldErrors;
-            // }
+            try {
+                await adEditSchema.parseAsync(val);
+            } catch (error) {
+                if (error instanceof ZodError) return error.formErrors.fieldErrors;
+            }
             return {};
         },
     });
@@ -36,6 +47,7 @@ const EditAd: FC<CreateAdProps> = ({ type = 'create' }) => {
                 border="1px solid rgba(0,0,0,0.2)"
                 boxShadow="md"
                 maxWidth="container.sm"
+                my={8}
                 p={[1, 4, 10]}
             >
                 <Heading pb={4} textAlign="center" textTransform="uppercase">
@@ -43,13 +55,52 @@ const EditAd: FC<CreateAdProps> = ({ type = 'create' }) => {
                 </Heading>
                 <form onSubmit={formik.handleSubmit}>
                     <InputField
+                        formik={formik}
+                        label="Body"
+                        name="body"
+                    />
+                    <InputField
+                        formik={formik}
+                        label="Pet Type"
+                        name="type"
+                    />
+                    <InputField
+                        formik={formik}
+                        label="Breed"
+                        name="breed"
+                    />
+                    <InputField
+                        autoComplete="name"
+                        formik={formik}
+                        label="Pet name"
+                        name="name"
+                    />
+                    <InputField
+                        formik={formik}
+                        label="Price"
+                        name="price"
+                        type="number"
+                    />
+                    <InputField
                         autoComplete="email"
                         formik={formik}
                         label="E-mail"
                         name="email"
-                        placeholder="Enter your e-mail..."
+                        type="email"
                     />
-                    <Flex justify="flex-end">
+                    <InputField
+                        formik={formik}
+                        label="Pone number"
+                        name="tel"
+                        type="tel"
+                    />
+                    <InputField
+                        formik={formik}
+                        label="Pet birthday"
+                        name="birthday"
+                        type="date"
+                    />
+                    <Flex justify="flex-end" mt={16}>
                         <Button
                             disabled={!formik.isValid}
                             isLoading={formik.isSubmitting}
