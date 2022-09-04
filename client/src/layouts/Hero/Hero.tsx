4f2ae47d89256516e943b4e7d95be6c93796d505
Anton <antklim89@mail.ru>
2022-09-04 12:02:21 +0300

    feat: update Hero

--- client/src/layouts/Hero/Hero.tsx
+++ client/src/layouts/Hero/Hero.tsx
@@ -1,30 +1,22 @@
-import { Container, Box } from '@chakra-ui/react';
+import { Container, Box, Text, Heading } from '@chakra-ui/react';
 import NextImage from 'next/image';
 import { FC } from 'react';
 
 import heroImage from './Hero.image.jpg';
 
-import Markdown from '~/components/Markdown';
-
 
 const Hero: FC = () => {
     return (
         <Box as="section" position="relative">
-            <Container
-                alignItems="center"
-                display="flex"
-                maxWidth="container.xl"
-                minHeight={320}
-            >
-                <Box
-                    backdropBlur="base"
-                    bg="rgba(255,255,255,0.5)"
-                    my={6}
-                    px={2}
-                    py={4}
-                    width="50%"
+            <Box bg="hsla(34, 100%, 15%, 0.5)" >
+                <Container
+                    maxWidth="container.xl"
+                    minHeight={320}
                 >
-                    <Markdown >
+                    <Heading as="h1" color="white" py={8} >
+                        Welcome
+                    </Heading>
+                    <Text color="white" >
                         Lorem ipsum dolor sit amet consectetur adipisicing elit.
                         Omnis eius et quidem, id in ipsam aperiam quia hic dolorum
                         assumenda placeat, itaque qui reprehenderit provident consectetur vel.
@@ -32,26 +24,26 @@ const Hero: FC = () => {
                         Lorem ipsum dolor sit amet consectetur adipisicing elit.
                         Repellat eveniet sint dignissimos earum sequi tempora fugit
                         asperiores quod exercitationem et. Veritatis.
-                    </Markdown>
-                </Box>
-                <Box
-                    bottom={0}
-                    left={0}
+                    </Text>
+                </Container>
+            </Box>
+            <Box
+                bottom={0}
+                left={0}
+                objectFit="cover"
+                position="absolute"
+                right={0}
+                top={0}
+                zIndex={-1}
+            >
+                <NextImage
+                    alt="hero"
+                    layout="fill"
                     objectFit="cover"
-                    position="absolute"
-                    right={0}
-                    top={0}
-                    zIndex={-1}
-                >
-                    <NextImage
-                        alt="hero"
-                        layout="fill"
-                        objectFit="cover"
-                        objectPosition="center"
-                        src={heroImage}
-                    />
-                </Box>
-            </Container>
+                    objectPosition="center"
+                    src={heroImage}
+                />
+            </Box>
         </Box>
     );
 };
