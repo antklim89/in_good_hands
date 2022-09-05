95eb15c73e2a56fa84f9e6373ddeca167fe78bbf
Anton <antklim89@mail.ru>
2022-09-05 14:33:30 +0300

    feat: create PetTypeSelect component

---
+++ client/src/components/PetTypeSelect/PetTypeSelect.tsx
@@ -0,0 +1,30 @@
+import { Select, SelectProps } from '@chakra-ui/react';
+import { animalsTypes } from '@in-good-hands/server/src/schemas/ad.schema';
+import { useRouter } from 'next/router';
+import { ChangeEventHandler, FC, useCallback } from 'react';
+
+import { setSearchParams } from '~/utils';
+
+
+const ALL = 'All';
+
+const PetTypeSelect: FC<SelectProps> = (props) => {
+    const router = useRouter();
+
+    const handleChange: ChangeEventHandler<HTMLSelectElement> = useCallback((e) => {
+        setSearchParams({ router, queryName: 'type', value: e.target.value, deleteValue: ALL });
+    }, [router.query]);
+
+    return (
+        <Select onChange={handleChange} {...props}>
+            <option value={ALL}>{ALL}</option>
+            {animalsTypes.map((animalsType) => (
+                <option key={animalsType} value={animalsType}>{animalsType}</option>
+            ))}
+        </Select>
+    );
+};
+
+export default PetTypeSelect;
+
+
