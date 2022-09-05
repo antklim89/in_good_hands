15e351b9f2ef54fa8be62116ed809cb0a753f952
Anton <antklim89@mail.ru>
2022-09-05 12:47:54 +0300

    feat: update pet age calculate

--- client/src/components/PetAge/PetAge.tsx
+++ client/src/components/PetAge/PetAge.tsx
@@ -4,13 +4,31 @@ import { FC } from 'react';
 import { PetAgeProps } from './PetAge.types';
 
 
-const PetAge: FC<PetAgeProps> = ({ birthday, ...props }) => {
-    const yearsOld = new Date().getFullYear() - new Date(birthday).getFullYear();
-    const monthOld = new Date().getMonth() - new Date(birthday).getMonth();
+const DAYS_IN_YEAR = 362.25;
+const MILLISECONDS_IN_YEAR = DAYS_IN_YEAR * 24 * 60 * 60 * 1000;
 
+const PetAge: FC<PetAgeProps> = ({ birthday, ...props }) => {
+    const yearsOld = (new Date().getTime() - new Date(birthday).getTime()) / MILLISECONDS_IN_YEAR;
+    const monthOld = (yearsOld - Math.floor(yearsOld)) * 12;
+
+    if (yearsOld < 0 || (yearsOld < 1 && monthOld < 1)) return (
+        <Text as="span" {...props}>
+            Less than a month
+        </Text>
+    );
+    if (yearsOld < 1 && monthOld > 1) return (
+        <Text as="span" {...props}>
+            {Math.floor(monthOld)} month old
+        </Text>
+    );
+    if (yearsOld > 1 && monthOld < 1) return (
+        <Text as="span" {...props}>
+            {Math.floor(yearsOld)} years old
+        </Text>
+    );
     return (
         <Text as="span" {...props}>
-            {yearsOld} years and {monthOld} month old
+            {Math.floor(yearsOld)} years and {Math.floor(monthOld)} month old
         </Text>
     );
 };
