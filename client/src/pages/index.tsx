75a2a74d7c2af576386fc47342ce047c6552664d
Anton <antklim89@mail.ru>
2022-09-06 00:46:39 +0300

    fix: ads in home page

--- client/src/pages/index.tsx
+++ client/src/pages/index.tsx
@@ -1,18 +1,30 @@
-import type { NextPage } from 'next';
+import { Ad } from '@in-good-hands/server/src/swagger';
+import type { GetServerSideProps, NextPage } from 'next';
 
 import Seo from '~/components/Seo';
 import AdsList from '~/layouts/AdsList';
 import Hero from '~/layouts/Hero';
+import { api } from '~/utils';
 
 
-const Home: NextPage = () => {
+interface Props {
+    ads: Ad.FindMany.ResponseBody
+}
+
+const Home: NextPage<Props> = ({ ads }) => {
     return (
         <>
             <Seo title="Home" />
             <Hero />
-            <AdsList ads={[]} />
+            <AdsList ads={ads} />
         </>
     );
+};
+
+export const getServerSideProps: GetServerSideProps<Props> = async () => {
+    const { data: ads } = await api().ad.findMany();
+
+    return { props: { ads } };
 };
 
 export default Home;
