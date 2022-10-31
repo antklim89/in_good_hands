7ced397609678c3b705bc45e37c860eb8780ece5
Anton <antklim89@mail.ru>
2022-10-31 23:54:09 +0300

    fix hasNext and add no ads message

--- client/src/layouts/MyAdsList/MyAdsList.tsx
+++ client/src/layouts/MyAdsList/MyAdsList.tsx
@@ -1,7 +1,4 @@
-import {
-    Box,
-    Button, Flex, Spinner,
-} from '@chakra-ui/react';
+import { Box, Button, Flex, Spinner, Text } from '@chakra-ui/react';
 import { FC, useCallback, useState } from 'react';
 import useSWR from 'swr';
 
@@ -11,35 +8,51 @@ import { api } from '~/utils';
 
 
 const MyAdsList: FC = () => {
-    const [hasNext, setHasNext] = useState(true);
-    const { data = [], mutate } = useSWR('my-ads', () => api().ad.findMyAds().then((d) => d.data));
+    const [hasNext, setHasNext] = useState(false);
 
-    const lastAdId = data?.slice?.().pop()?.id;
+    const { data: ads = [], mutate } = useSWR('my-ads', async () => {
+        const { data } = await api().ad.findMyAds();
+        if (data.length > 0) setHasNext(true);
+        return data;
+    });
+
+    const lastAdId = ads?.slice?.().pop()?.id;
 
     const handleFetchMore = useCallback(async () => {
         const nextAds = await api().ad.findMyAds({ cursor: lastAdId }).then((d) => d.data);
         if (nextAds.length <= 1) setHasNext(false);
-        mutate([...data || [], ...nextAds], { revalidate: false });
+        mutate([...ads || [], ...nextAds], { revalidate: false });
     }, [lastAdId]);
 
-    if (!data) return <Spinner display="flex" justifyContent="center" />;
+    if (!ads) return <Spinner display="flex" justifyContent="center" />;
     return (
         <div>
             <Box>
-                {data.map((ad) => (
+                {ads.map((ad) => (
                     <MyAdsListItem {...ad} key={ad.id} />
                 ))}
             </Box>
 
-            {hasNext
-                ? (
-                    <Flex justifyContent="center" mt={8}>
-                        <Button variant="outline" onClick={handleFetchMore}>
-                            Show More
-                        </Button>
-                    </Flex>
-                )
-                : null}
+            {
+                ads.length === 0
+                    ? (
+                        <Text my={4} textAlign="center">
+                            You have not created  any ads yet.
+                        </Text>
+                    )
+                    : null
+            }
+            {
+                hasNext
+                    ? (
+                        <Flex justifyContent="center" mt={8}>
+                            <Button variant="outline" onClick={handleFetchMore}>
+                                Show More
+                            </Button>
+                        </Flex>
+                    )
+                    : null
+            }
         </div>
     );
 };
