5b2429744c2b6647136dff1ea423b8543a326edf
Anton <antklim89@mail.ru>
2022-11-01 00:11:19 +0300

    add no favorites message

--- client/src/layouts/MyFavorites/MyFavorites.tsx
+++ client/src/layouts/MyFavorites/MyFavorites.tsx
@@ -8,29 +8,43 @@ import { api } from '~/utils';
 
 
 const MyFavorites: FC = () => {
-    const { data = [] } = useSWR('my-favorites', () => api().favorites.findMany().then((d) => d.data));
+    const { data: favorites = [] } = useSWR('my-favorites', () => api().favorites.findMany().then((d) => d.data));
 
     return (
         <Box>
-            {data.map(({ id, ad }) => (
-                <Link href={`/ads/${id}`} key={id}>
-                    <Box as="a" cursor="pointer">
-                        <HStack flexDir={['column', null, 'row']} py={4}>
-                            <Flex mr="auto" textTransform="uppercase">
-                                <Text px={4} py={0}>{ad.type}</Text>
-                                <Text px={4} py={0}>{ad.breed}</Text>
+            {
+                favorites.length === 0
+                    ? (
+                        <Text my={4} textAlign="center">
+                            You have not add favorites ads yet.
+                        </Text>
+                    )
+                    : null
+            }
+            {favorites.map(({ id, ad }) => (
+                <Box key={id}>
+                    <HStack flexDir={['column', null, 'row']} py={4}>
+                        <Link href={`/ads/${ad.id}`}>
+                            <Flex
+                                alignItems="center"
+                                as="a"
+                                cursor="pointer"
+                                mr="auto"
+                                textTransform="uppercase"
+                            >
+                                <Text px={4} py={0}>{ad.type} <br /> {ad.breed}</Text>
                                 <Text px={4} py={0}>{ad.name}</Text>
                                 <Text px={4} py={0}>{ad.price}$</Text>
                             </Flex>
-                            <FavoriteButton
-                                inFavorites
-                                adId={ad.id}
-                                iconProps={{ fontSize: 'xl' }}
-                                size="xs"
-                            />
-                        </HStack>
-                    </Box>
-                </Link>
+                        </Link>
+                        <FavoriteButton
+                            inFavorites
+                            adId={ad.id}
+                            iconProps={{ fontSize: 'xl' }}
+                            size="xs"
+                        />
+                    </HStack>
+                </Box>
             ))}
         </Box>
     );
