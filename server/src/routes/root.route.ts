9974772986e1082d919e9625cd38448066687e25
Anton <antklim89@mail.ru>
2022-11-18 21:56:03 +0300

    fix

--- server/src/routes/root.route.ts
+++ server/src/routes/root.route.ts
@@ -3,11 +3,6 @@ import { FastifyInstance } from 'fastify';
 
 export default async function route (app: FastifyInstance) {
     app.get('/', async () => {
-        await app.prisma.user.create({ data: {
-            email: `email${Math.random()}`,
-            hash: 'xxx',
-            name: 'Name',
-        } });
         return { msg: 'Hello' };
     });
 }
