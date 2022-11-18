b08957bad42b15c9c236b01852eeca6986dec5f3
Anton <antklim89@mail.ru>
2022-11-18 22:54:32 +0300

    update

--- server/src/server.ts
+++ server/src/server.ts
@@ -4,20 +4,17 @@ import app from './app';
 import { generateSwaggerTypes } from './utils';
 
 
-const { PORT = 8000 } = process.env;
+const { PORT = 8000, HOST = '0.0.0.0' } = process.env;
 
-export const start = async () => {
-    try {
-        const prisma = new PrismaClient();
+const prisma = new PrismaClient();
 
-        app.prisma = prisma;
+app.prisma = prisma;
 
-        await app.listen({ port: Number(PORT), host: '0.0.0.0' }, () => {
-            generateSwaggerTypes(app);
-        });
-    } catch (err) {
-        app.log.error(err);
+app.listen({ port: Number(PORT), host: HOST }, (err, address) => {
+    if (err) {
+        app.log.info('Error', err);
         process.exit(1);
     }
-};
-start();
+    if (process.env.NODE_ENV !== 'production') generateSwaggerTypes(app);
+    app.log.info(`Server is now listening on ${address}`);
+});
