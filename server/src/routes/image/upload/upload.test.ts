a2a0950ee10f685670eac74503f6699f9d077711
Anton <antklim89@mail.ru>
2023-05-01 21:13:25 +0300

    image limit tests

--- server/src/routes/image/upload/upload.test.ts
+++ server/src/routes/image/upload/upload.test.ts
@@ -5,7 +5,7 @@ import fs from 'fs-extra';
 
 import { UPLOAD_IMAGES_BASE_PATH } from '@/constants';
 import { Image } from '@/swagger';
-import { init } from '@/test';
+import { createImage, init } from '@/test';
 import { generateJWT } from '@/utils';
 
 
@@ -16,6 +16,11 @@ const defaultOptions: import('light-my-request').InjectOptions = {
     method: 'POST',
 };
 
+jest.mock('../../../shareConstants', () => ({
+    UPLOAD_IMAGES_LIMIT: 21,
+    IMAGE_WIDHT: 1024,
+    IMAGE_HEIGHT: 768,
+}));
 
 const image = () => fs.createReadStream(resolve(process.cwd(), './src/test/test.image.jpg'));
 
@@ -36,6 +41,7 @@ describe('POST /image/upload', () => {
         const response = await app.inject({ ...defaultOptions, ...form, query });
         const uploadedImageResponse: Image.Upload.ResponseBody = response.json();
 
+
         const uploadedImage = await prisma.image.findUnique({
             where: { id: uploadedImageResponse.id },
         });
@@ -44,6 +50,27 @@ describe('POST /image/upload', () => {
         expect(uploadedImage).toHaveProperty('src');
         expect(uploadedImage).toHaveProperty('thumbnail');
         expect(uploadedImage).toHaveProperty('adId', db().ads[0].id);
+    });
+
+    it('should not upload new image over limit', async () => {
+        await fs.rm(UPLOAD_IMAGES_BASE_PATH, { recursive: true, force: true });
+        await prisma.image.create({ data: createImage({
+            ad: { connect: { id: db().ads[0].id } },
+        }) });
+        const form = formAutoContent({
+            image: image(),
+        });
+
+        const query = {
+            adId: String(db().ads[0].id),
+        };
+
+        form.headers.authentication = generateJWT(db().users[0]).token;
+
+        const response = await app.inject({ ...defaultOptions, ...form, query });
+        const uploadedImageResponse = response.json();
+
+        expect(uploadedImageResponse.statusCode).toEqual(400);
     });
 
     it('should not upload image from wrong user', async () => {
