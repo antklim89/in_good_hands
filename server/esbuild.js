bae8b6321993a19ef70fcea385933416fec9e92b
Anton <antklim89@mail.ru>
2024-01-08 13:17:35 +0300

    install esbuild

---
+++ server/esbuild.js
@@ -0,0 +1,17 @@
+import * as esbuild from 'esbuild';
+import { nodeExternalsPlugin } from 'esbuild-node-externals';
+
+
+await esbuild.build({
+    entryPoints: ['src/server.ts'],
+    bundle: true,
+    outfile: 'output/server.js',
+    platform: 'node',
+    target: 'node20',
+    format: 'esm',
+    minify: false,
+    minifyIdentifiers: false,
+    minifySyntax: false,
+    minifyWhitespace: false,
+    plugins: [nodeExternalsPlugin()],
+});
