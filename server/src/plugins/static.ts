e8992cf0a83aefc5537935178ac7be50362850f8
Anton <antklim89@mail.ru>
2024-01-08 11:50:49 +0300

    refactor: replace require with import

--- server/src/plugins/static.ts
+++ server/src/plugins/static.ts
@@ -5,7 +5,7 @@ import { UPLOAD_BASE_PATH } from '@/constants';
 
 
 export default fp(async (app) => {
-    app.register(require('@fastify/static'), {
+    app.register(import('@fastify/static'), {
         root: UPLOAD_BASE_PATH,
         prefix: '/upload',
     });
--- server/src/plugins/swagger.ts
+++ server/src/plugins/swagger.ts
@@ -2,9 +2,9 @@ import fp from 'fastify-plugin';
 
 
 export default fp(async (app) => {
-    await app.register(require('@fastify/swagger'));
+    await app.register(import('@fastify/swagger'));
 
-    await app.register(require('@fastify/swagger-ui'), {
+    await app.register(import('@fastify/swagger-ui'), {
         routePrefix: '/documentation',
         uiConfig: {
             docExpansion: 'full',
