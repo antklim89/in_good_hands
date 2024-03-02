0334099090557ed73c3b1b14df5f207289b13e70
Anton <antklim89prog@mail.ru>
2024-03-02 20:02:09 +0300

    build: add rewrites

--- client/next.config.js
+++ client/next.config.js
@@ -1,5 +1,6 @@
 const { NEXT_PUBLIC_API_URL } = process.env;
 
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     reactStrictMode: true,
@@ -12,6 +13,14 @@ const nextConfig = {
         ],
     },
     transpilePackages: ['@in-good-hands/server'],
+    async rewrites() {
+        return [
+            {
+                source: '/server/:path*',
+                destination: `${NEXT_PUBLIC_API_URL}/:path*`,
+            },
+        ];
+    },
 };
 
 module.exports = nextConfig;
