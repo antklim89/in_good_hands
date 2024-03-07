7d6b732e9709f0bd2e3752b85a075761a111c37b
Anton <antklim89prog@mail.ru>
2024-03-07 21:10:10 +0300

    chore: add init postgres migration

---
+++ server/prisma/migrations/20240307171156_init/migration.sql
@@ -0,0 +1,73 @@
+-- CreateTable
+CREATE TABLE "User" (
+    "id" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "hash" TEXT NOT NULL,
+    "tel" TEXT DEFAULT '',
+    "whatsapp" TEXT DEFAULT '',
+    "telegram" TEXT DEFAULT '',
+
+    CONSTRAINT "User_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Ad" (
+    "id" SERIAL NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+    "name" TEXT,
+    "type" TEXT NOT NULL DEFAULT 'cat',
+    "breed" TEXT NOT NULL,
+    "description" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "tel" TEXT NOT NULL,
+    "whatsapp" TEXT DEFAULT '',
+    "telegram" TEXT DEFAULT '',
+    "price" DOUBLE PRECISION NOT NULL,
+    "birthday" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "isPublished" BOOLEAN NOT NULL,
+    "ownerId" TEXT NOT NULL,
+
+    CONSTRAINT "Ad_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Image" (
+    "id" SERIAL NOT NULL,
+    "src" TEXT NOT NULL,
+    "thumbnail" TEXT NOT NULL,
+    "adId" INTEGER NOT NULL,
+
+    CONSTRAINT "Image_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Favorites" (
+    "id" SERIAL NOT NULL,
+    "adId" INTEGER NOT NULL,
+    "ownerId" TEXT NOT NULL,
+
+    CONSTRAINT "Favorites_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
+
+-- CreateIndex
+CREATE INDEX "Favorites_adId_ownerId_idx" ON "Favorites"("adId", "ownerId");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Favorites_adId_ownerId_key" ON "Favorites"("adId", "ownerId");
+
+-- AddForeignKey
+ALTER TABLE "Ad" ADD CONSTRAINT "Ad_ownerId_fkey" FOREIGN KEY ("ownerId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Image" ADD CONSTRAINT "Image_adId_fkey" FOREIGN KEY ("adId") REFERENCES "Ad"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Favorites" ADD CONSTRAINT "Favorites_adId_fkey" FOREIGN KEY ("adId") REFERENCES "Ad"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Favorites" ADD CONSTRAINT "Favorites_ownerId_fkey" FOREIGN KEY ("ownerId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
